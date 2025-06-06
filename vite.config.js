a6b5fcd7cdd83b6e9318bfbb828f466f95af268e
sayali <jsayali952@gmail.com>
2025-06-06 15:24:08 +0530

    property added

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
    server: {
     port: 5174,
+    proxy: {
+      '/api': 'http://localhost:5000', // ✅ proxy MUST be inside server
+    },
   },
 })
